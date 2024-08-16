376fa863f5be8d711e037f4913c806eb18729c33
NitishGreens <nitishgreens@gmail.com>
2024-08-16 11:25:25 +0530

    Member2 added some scripts

--- src/org/emp/Employee.java
+++ src/org/emp/Employee.java
@@ -6,6 +6,10 @@ public class Employee {
 		System.out.println("add");
 	}
 
+	public void div() {
+		System.out.println("div");
+	}
+
 	public static void main(String[] args) {
 
 		Employee e = new Employee();
